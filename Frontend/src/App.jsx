fef12ea0b23826328de1641131a5e3afbd33f31e
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-12-11 08:02:43 +0000

    redux

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className='text-3xl font-bold text-center bg-rose-700'>App</div>
+    <div className='text-2xl font-bold text-center bg-rose-700'>App</div>
   )
 }
 
