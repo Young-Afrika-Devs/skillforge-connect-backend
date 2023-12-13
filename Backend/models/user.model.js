a79ba0aae173b632395260615d181e79a84ddebe
LevittC17 <LeewednesdayC17@protonmail.com>
2023-12-13 14:02:59 +0300

    created user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        validate: {
+            validator: value => value.length >= 3,
+            message: () => `Username must be at least 3 characters long`
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: value =>{
+            const regex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            return regex.test(value);
+        },
+        message: () => `Please enter a valid email!`
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate: {
+            validator: value => value.legth >= 8,
+            message: () => `Password must be at least 8 characters long`
+        }
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
